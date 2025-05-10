765e0dee7635c641b9d0218d5d65ab0cb42a7d34
moh.faathirashshaff <moh.faathirashshaff2@gmail.com>
2025-05-11 00:15:22 +0800

    Pertemuan 4

--- laravel-praktikum/app/Providers/AppServiceProvider.php
+++ laravel-praktikum/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,48 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('view-student', function (User $user) {
+            if ($user->role === "admin" || $user->role === "guest") {
+                return true;
+            }
+            return false;
+        });
+    
+        Gate::define('store-student', function (User $user) {
+            if ($user->role === "admin") {
+                return true;
+            }
+            return false;
+        });
+    
+        Gate::define('edit-student', function (User $user) {
+            if ($user->role === "admin") {
+                return true;
+            }
+            return false;
+        });
+    
+        Gate::define('destroy-student', function (User $user) {
+            if ($user->role === "admin") {
+                return true;
+            }
+            return false;
+        });
+
+        Gate::define('view-major', function (User $user) {
+            return in_array($user->role, ['admin', 'guest']);
+        });
+    
+        Gate::define('store-major', function (User $user) {
+            return $user->role === "admin";
+        });
+    
+        Gate::define('edit-major', function (User $user) {
+            return $user->role === "admin";
+        });
+    
+        Gate::define('destroy-major', function (User $user) {
+            return $user->role === "admin";
+        });
     }
 }
